af2570281c55ba68ef415f53aca91631e40b097d
CarloCapuz <36867035+CarloCapuz@users.noreply.github.com>
2019-04-18 14:24:17 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Folder_Security_Permission
+{
+    partial class FSPform
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxDrive = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxPermission = new System.Windows.Forms.ComboBox();
+            this.labelOutputPerm = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonModify = new System.Windows.Forms.Button();
+            this.comboBoxUserName = new System.Windows.Forms.ComboBox();
+            this.textBoxFolderName = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.listBoxDriveContent = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Drive:";
+            // 
+            // comboBoxDrive
+            // 
+            this.comboBoxDrive.FormattingEnabled = true;
+            this.comboBoxDrive.Items.AddRange(new object[] {
+            "C",
+            "E",
+            "N",
+            "P",
+            "U",
+            "Z",
+            "Y"});
+            this.comboBoxDrive.Location = new System.Drawing.Point(169, 12);
+            this.comboBoxDrive.Name = "comboBoxDrive";
+            this.comboBoxDrive.Size = new System.Drawing.Size(237, 21);
+            this.comboBoxDrive.TabIndex = 1;
+            this.comboBoxDrive.SelectedIndexChanged += new System.EventHandler(this.comboBoxDrive_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Drive Contents:";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutput.Location = new System.Drawing.Point(12, 439);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(0, 20);
+            this.labelOutput.TabIndex = 4;
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreate.Location = new System.Drawing.Point(169, 389);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 5;
+            this.buttonCreate.Text = "&Create";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(331, 389);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 6;
+            this.buttonDelete.Text = "&Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Security Permission:";
+            // 
+            // comboBoxPermission
+            // 
+            this.comboBoxPermission.FormattingEnabled = true;
+            this.comboBoxPermission.Items.AddRange(new object[] {
+            "Full Control",
+            "Modify",
+            "Read & Execute",
+            "Read",
+            "Write",
+            " "});
+            this.comboBoxPermission.Location = new System.Drawing.Point(169, 324);
+            this.comboBoxPermission.Name = "comboBoxPermission";
+            this.comboBoxPermission.Size = new System.Drawing.Size(237, 21);
+            this.comboBoxPermission.TabIndex = 8;
+            // 
+            // labelOutputPerm
+            // 
+            this.labelOutputPerm.AutoSize = true;
+            this.labelOutputPerm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOutputPerm.Location = new System.Drawing.Point(12, 474);
+            this.labelOutputPerm.Name = "labelOutputPerm";
+            this.labelOutputPerm.Size = new System.Drawing.Size(0, 20);
+            this.labelOutputPerm.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(70, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "User Name:";
+            // 
+            // buttonModify
+            // 
+            this.buttonModify.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonModify.Location = new System.Drawing.Point(250, 389);
+            this.buttonModify.Name = "buttonModify";
+            this.buttonModify.Size = new System.Drawing.Size(75, 23);
+            this.buttonModify.TabIndex = 12;
+            this.buttonModify.Text = "&Modify";
+            this.buttonModify.UseVisualStyleBackColor = true;
+            this.buttonModify.Click += new System.EventHandler(this.buttonModify_Click);
+            // 
+            // comboBoxUserName
+            // 
+            this.comboBoxUserName.FormattingEnabled = true;
+            this.comboBoxUserName.Items.AddRange(new object[] {
+            "aconn",
+            "aschwab",
+            "ccail",
+            "flance",
+            "jdesharnais",
+            "myusuf",
+            "bsteiger"});
+            this.comboBoxUserName.Location = new System.Drawing.Point(169, 353);
+            this.comboBoxUserName.Name = "comboBoxUserName";
+            this.comboBoxUserName.Size = new System.Drawing.Size(237, 21);
+            this.comboBoxUserName.TabIndex = 14;
+            // 
+            // textBoxFolderName
+            // 
+            this.textBoxFolderName.Location = new System.Drawing.Point(169, 298);
+            this.textBoxFolderName.Name = "textBoxFolderName";
+            this.textBoxFolderName.Size = new System.Drawing.Size(237, 20);
+            this.textBoxFolderName.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(59, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 20);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Folder Name:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdd.Location = new System.Drawing.Point(169, 269);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 17;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.Location = new System.Drawing.Point(250, 269);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 18;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRefresh.Location = new System.Drawing.Point(88, 124);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 19;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonList_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(88, 153);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 20;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // listBoxDriveContent
+            // 
+            this.listBoxDriveContent.FormattingEnabled = true;
+            this.listBoxDriveContent.Location = new System.Drawing.Point(169, 39);
+            this.listBoxDriveContent.Name = "listBoxDriveContent";
+            this.listBoxDriveContent.Size = new System.Drawing.Size(237, 225);
+            this.listBoxDriveContent.TabIndex = 21;
+            // 
+            // FSPform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(481, 523);
+            this.Controls.Add(this.listBoxDriveContent);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFolderName);
+            this.Controls.Add(this.comboBoxUserName);
+            this.Controls.Add(this.buttonModify);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelOutputPerm);
+            this.Controls.Add(this.comboBoxPermission);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxDrive);
+            this.Controls.Add(this.label1);
+            this.Name = "FSPform";
+            this.Text = "Folder Security Permission";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxDrive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxPermission;
+        private System.Windows.Forms.Label labelOutputPerm;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonModify;
+        private System.Windows.Forms.ComboBox comboBoxUserName;
+        private System.Windows.Forms.TextBox textBoxFolderName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.ListBox listBoxDriveContent;
+    }
+}
+
